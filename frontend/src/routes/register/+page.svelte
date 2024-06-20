b11fb504eba8cf5a6f38c8239177c53eaf07bc2e
Stelviaa <forestiesacha@gmail.com>
2024-06-20 08:29:15 +0200

    add login and register

---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    let password1 = "";
+    let password2 = "";
+
+    $: samePassword = (password1 === password2 || password2 === "");
+</script>
+
+<div class="container-fluid" style="height:100vh;">
+    <div class="d-flex justify-content-center align-items-center" style="height:100%;">
+        <div class="p-5 border rounded">
+            <div class="mb-3">
+                <label for="Pseudonyme" class="form-label text-light"><h5>Pseudonyme</h5></label>
+                <input type="text" class="form-control" id="Pseudonyme" placeholder="Enter pseudonyme">
+            </div>
+            <div class="mb-3">
+                <label for="password1" class="form-label text-light"><h5>Password</h5></label>
+                <input bind:value={password1} type="password" class="form-control" id="password1" placeholder="Enter password">
+            </div>
+            <div class="mb-3">
+                <label for="password2" class="form-label text-light"><h5>Confirm your password</h5></label>
+                <input bind:value={password2} type="password" class="form-control" id="password2" placeholder="Enter password">
+                {#if samePassword}
+                    <p class="text-light">yesss</p>
+                {:else}
+                    <p class="text-light">Nooo</p>
+                {/if}
+            </div>
+            <div class="btn-group-vertical col-12" role="group" aria-label="Vertical button group">
+                <button type="button" class="m-1 btn btn-primary">Create my account</button>
+            </div>
+        </div>
+    </div>
+</div>
--- frontend/src/routes/tournament/+page.svelte
+++ frontend/src/routes/tournament/+page.svelte
@@ -1,16 +1,68 @@
+<script>
+    const img = new URL('$lib/assets/pong.png', import.meta.url).href
+    const img1 = new URL('$lib/assets/game2.png', import.meta.url).href
+    
+</script>
+
 <div class="container">
     <div class="row mt-5">
         <div class="d-flex justify-content-center border-bottom">
-            <button class="btn btn-primary mb-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"><i class="bi-plus-square-fill" style="font-size: 1.6rem; color: cornflowerblue;"></i><p class="mb-1">New tournament</p></button>
+            <button class="btn btn-primary mb-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><i class="bi-plus-square-fill" style="font-size: 1.6rem; color: cornflowerblue;"></i><p class="mb-1">New tournament</p></button>
         </div>
-        <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            ...
-        </div>
+        <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+            <div class="offcanvas-header">
+                <h3 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Tournament Creation</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div>
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput" class="form-label"><h4>Tournament's name</h4></label>
+                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter name">
+                    </div>
+                </div>
+                <div class="">
+                    <h4>Size of tournament</h4>
+                    <div class="row mt-3">
+                        <div class="col-4">
+                            <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+                            <label class="btn btn-secondary" for="option1">4 players</label>
+                        </div>
+                        <div class="col-4">    
+                            <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+                            <label class="btn btn-secondary" for="option2">8 players</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-4">
+                    <h4>Choose your game</h4>
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src={img} class="d-block w-100 mt-2" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src={img1} class="d-block w-100 mt-2" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center mb-4">
+                <button type="button" class="btn btn-dark btn-lg mb-5 p-3">Create Tournament</button>
+            </div>
         </div>
     </div>
 </div>
