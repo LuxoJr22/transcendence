7167b84108e2498847d8e5209b0372e2b01327f5
olmayto <mayou1369ios@gmail.com>
2024-07-13 10:38:34 +0200

    Add default Tokens views and user details view

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import TriplumUser
 from .serializers import TriplumUserSerializer
 
@@ -8,3 +9,15 @@ class TriplumRegisterView(generics.CreateAPIView):
 	queryset = TriplumUser.objects.all()
 	serializer_class = TriplumUserSerializer
 	permission_classes = [AllowAny] # Access to this view
+
+class TriplumUserDetailView(APIView):
+	permission_classes = [IsAuthenticated]
+	
+	def get(self, request):
+		user = request.user
+		data = {
+			'id': user.id,
+			'username': user.username,
+			'email': user.email,
+		}
+		return Response(data)
