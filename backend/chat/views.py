3738246ca53ef93c4673b2d3d2119692d1c28b69
mpelazza <mpelazza@desktop.com>
2024-09-20 16:01:09 +0200

    add backend chat (need more tests)

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Message
+from .serializers import MessageSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class MessageListView(generics.ListAPIView):
+	serializer_class = MessageSerializer
+	permission_classes = [IsAuthenticated]
+
+	def get_queryset(self):
+		sender = self.request.user
+		receiver_username = self.kwargs['username']
+		return Message.objects.filter(sender=sender, receiver__username=receiver_username) | \
+				Message.objects.filter(sender__username=receiver_username, receiver=sender)
