56d9d5e6a61b560b91bef24bf3f2e8d2958f6a66
mpelazza <mpelazza@desktop.com>
2024-09-27 15:16:44 +0200

    clean useless backend for join tournament

--- backend/tournament/urls.py
+++ backend/tournament/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import CreateTournamentView, JoinTournamentView
+from .views import CreateTournamentView
 
 urlpatterns = [
 	path('tournament/create/', CreateTournamentView.as_view(), name='create_tournament'),
-	path('tournament/join/<int:tournament_id>/', JoinTournamentView.as_view(), name='join_tournament'),
 ]
