2fbd1da4bfa75f2f22b2dd02ea925af335ce6943
mpelazza <mpelazza@desktop.com>
2024-10-01 14:16:51 +0200

    Update requirements / move django SECRET_KEY from settings to env

--- backend/pong/urls.py
+++ backend/pong/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PongMatchHistoryView, SkinsView
 
 urlpatterns = [
-	path('pong/skins/<int:game_id>', SkinsView.as_view(), name='oponent_skin'),
+	path('pong/skins/<int:game_id>', SkinsView.as_view(), name='players_skin'),
 	path('pong/history/', PongMatchHistoryView.as_view(), name='pong_match_history'),
 ]
