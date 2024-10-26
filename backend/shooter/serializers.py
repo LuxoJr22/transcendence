089075381ef59639d140f0ede79c741cd83e7a34
Matteo Pelazza <mpelazza@c1r3p2.42nice.fr>
2024-10-26 16:06:17 +0200

    ShooterHistory

--- backend/shooter/serializers.py
+++ backend/shooter/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import ShooterMatch
+from .models import ShooterMatch, PlayerScore
+
+class PlayerScoreSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = PlayerScore
+		fields = ['player_id', 'score']
 
 class ShooterMatchSerializer(serializers.ModelSerializer):
+	scores = PlayerScoreSerializer(many=True)
+
 	class Meta:
 		model = ShooterMatch
 		fields = [ 'id', 'players', 'scores', 'winner', 'elo', 'match_date']
