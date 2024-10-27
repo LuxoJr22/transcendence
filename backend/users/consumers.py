91814f7ba1c85e9f10bfc3309c1e056111068716
olmayto <mayou1369ios@gmail.com>
2024-10-27 22:26:16 +0100

    User status websocket now handle multiple-tab connections

--- backend/users/consumers.py
+++ backend/users/consumers.py
@@ -3,9 +3,14 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
 class UserStatusConsumer(WebsocketConsumer):
+	connection_counts = {}
+
 	def connect(self):
 		self.user = self.scope["user"]
 		if self.user.is_authenticated:
+			if self.user.id not in self.connection_counts:
+				self.connection_counts[self.user.id] = 0
+			self.connection_counts[self.user.id] += 1
 			async_to_sync(self.channel_layer.group_add)(
 				"online_users",
 				self.channel_name
@@ -15,15 +20,16 @@ class UserStatusConsumer(WebsocketConsumer):
 				self.channel_name
 			)
 			self.accept()
-			self.update_user_status(online=True)
-			self.group_send("online_users", {
-				"type": "user_online",
-				"user_id": self.user.id,
-				"online": True
-			})
+			self.update_user_status()
+			# self.group_send("online_users", {
+			# 	"type": "user_online",
+			# 	"user_id": self.user.id,
+			# 	"online": True
+			# })
 
 	def disconnect(self, close_code):
 		if self.user.is_authenticated:
+			self.connection_counts[self.user.id] -= 1
 			async_to_sync(self.channel_layer.group_discard)(
 				"online_users",
 				self.channel_name
@@ -32,14 +38,18 @@ class UserStatusConsumer(WebsocketConsumer):
 				f"user_{self.user.id}",
 				self.channel_name
 			)
-			self.update_user_status(online=False)
-			self.group_send("online_users", {
-				"type": "user_online",
-				"user_id": self.user.id,
-				"online": False
-			})
+			self.update_user_status()
+			# self.group_send("online_users", {
+			# 	"type": "user_online",
+			# 	"user_id": self.user.id,
+			# 	"online": False
+			# })
 
-	def update_user_status(self, online):
+	def update_user_status(self):
+		if self.connection_counts[self.user.id] == 0:
+			online = False
+		else:
+			online = True
 		self.user.is_online = online
 		self.user.save(update_fields=['is_online'])
 
